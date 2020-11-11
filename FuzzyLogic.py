a035da67ccea4aafa7b9f42cbd3da7652404d9ab
Mohammaddaffaharis <mohammaddaffaharis@gmail.com>
2020-11-11 22:51:39 +0700

    hayulau

--- FuzzyLogic.py
+++ FuzzyLogic.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
 file = pd.read_excel('Mahasiswa.xls')
 pengeluaran = []
 penghasilan = []
@@ -9,50 +7,10 @@ for i in file.index:
     pengeluaran.append(file['Pengeluaran'][i])
     penghasilan.append(file['Penghasilan'][i])
 
-
-# high,average,low
-def pengeluaranTinggi(pengeluaran):
-    max = 10
-    min = 8
-    if(pengeluaran > max):
-        return 1
-    elif(pengeluaran > min and pengeluaran <= max):
-        return (pengeluaran - min) / (max - min)
-    elif(pengeluaran <= min):
-        return 0
-def pengeluaranRendah(pengeluaran):
-    max = 5
-    min = 3
-    if(pengeluaran > max):
-        return 0
-    elif(pengeluaran > min and pengeluaran <= max):
-        return (max - pengeluaran) / (max - min)
-    elif(pengeluaran <= min):
-        return 1
-def pengeluaranStandar(pengeluaran):
-    max = 7.5
-    min = 5.5
-    maxUnCom = 9
-    minUnCom = 4.5
-    if(pengeluaran <= minUnCom or pengeluaran > maxUnCom):
-        return 0
-    elif(pengeluaran > minUnCom and pengeluaran <= min):
-        return (pengeluaran-minUnCom) / (min-minUnCom)
-    elif(pengeluaran > min and pengeluaran <= max):
-        return 1
-    elif(pengeluaran > max and pengeluaran <= maxUnCom):
-        return (maxUnCom-pengeluaran) / (maxUnCom-max)
-def klasifikasiPengeluaran(pengeluaran):
-    result = []
-    result.append(pengeluaranRendah(pengeluaran))
-    result.append(pengeluaranStandar(pengeluaran))
-    result.append(pengeluaranTinggi(pengeluaran))
-    return result
-
 #upper,middle,bottom
 def penghasilanTinggi(penghasilan):
     max = 19
-    min = 14
+    min = 12
     if(penghasilan > max):
         return 1
     elif(penghasilan > min and penghasilan <= max):
@@ -69,10 +27,10 @@ def penghasilanRendah(penghasilan):
     elif(penghasilan <= min):
         return 1
 def penghasilanStandar(penghasilan):
-    max = 13.5
-    min = 7.5
-    maxUnCom = 17
-    minUnCom = 5
+    max = 13
+    min = 8
+    maxUnCom = 12
+    minUnCom = 7
     if(penghasilan <= minUnCom or penghasilan > maxUnCom):
         return 0
     elif(penghasilan > minUnCom and penghasilan <= min):
@@ -85,7 +43,8 @@ def klasifikasiPenghasilan(penghasilan):
     result = []
     result.append(penghasilanRendah(penghasilan))
     result.append(penghasilanStandar(penghasilan))
-    result.append(pengeluaranStandar(penghasilan))
+    result.append(penghasilanTinggi(penghasilan))
+    return result
 def printPengeluarannPenghasilan():
     x = [3, 4.5, 5, 5.5, 7.5, 8, 9, 10]
     y1 = [0, 0, 0, 0, 0, 1, 1, 1]
@@ -109,22 +68,98 @@ def printPengeluarannPenghasilan():
     plt.legend()
     plt.show()
 
-def fuzzyTable(fuzzy1, fuzzy2):
-    if (fuzzy1 == 'bottom') and (fuzzy2 == 'low'):
-        return 'considered'
-    elif (fuzzy1 == 'bottom') and (fuzzy2 == 'average'):
-        return 'accepted'
-    elif (fuzzy1 == 'bottom') and (fuzzy2 == 'high'):
-        return 'accepted'
-    elif (fuzzy1 == 'middle') and (fuzzy2 == 'low'):
-        return 'considered'
-    elif (fuzzy1 == 'middle') and (fuzzy2 == 'average'):
-        return 'considered'
-    elif (fuzzy1 == 'middle') and (fuzzy2 == 'high'):
-        return 'accepted'
-    elif (fuzzy1 == 'upper') and (fuzzy2 == 'low'):
-        return 'rejected'
-    elif (fuzzy1 == 'upper') and (fuzzy2 == 'average'):
-        return 'rejected'
-    elif (fuzzy1 == 'upper') and (fuzzy2 == 'high'):
-        return 'considered'
+# high,average,low
+def pengeluaranTinggi(pengeluaran):
+    max = 10
+    min = 9
+    if(pengeluaran > max):
+        return 1
+    elif(pengeluaran > min and pengeluaran <= max):
+        return (pengeluaran - min) / (max - min)
+    elif(pengeluaran <= min):
+        return 0
+def pengeluaranRendah(pengeluaran):
+    max = 5
+    min = 3
+    if(pengeluaran > max):
+        return 0
+    elif(pengeluaran > min and pengeluaran <= max):
+        return (max - pengeluaran) / (max - min)
+    elif(pengeluaran <= min):
+        return 1
+def pengeluaranStandar(pengeluaran):
+    max = 8
+    min = 6
+    maxUnCom = 9
+    minUnCom = 5
+    if(pengeluaran <= minUnCom or pengeluaran > maxUnCom):
+        return 0
+    elif(pengeluaran > minUnCom and pengeluaran <= min):
+        return (pengeluaran-minUnCom) / (min-minUnCom)
+    elif(pengeluaran > min and pengeluaran <= max):
+        return 1
+    elif(pengeluaran > max and pengeluaran <= maxUnCom):
+        return (maxUnCom-pengeluaran) / (maxUnCom-max)
+def klasifikasiPengeluaran(pengeluaran):
+    result = []
+    result.append(pengeluaranRendah(pengeluaran))
+    result.append(pengeluaranStandar(pengeluaran))
+    result.append(pengeluaranTinggi(pengeluaran))
+    return result
+
+def inferenceTable(fuzzyPenghasilan, fuzzyPengeluaran):
+    upper = fuzzyPenghasilan[2]
+    middle = fuzzyPenghasilan[1]
+    bottom = fuzzyPenghasilan[0]
+    high = fuzzyPengeluaran[2]
+    average = fuzzyPengeluaran[1]
+    low = fuzzyPengeluaran[0]
+
+    arrAccepted = []
+    arrConsidered = []
+    arrRejected = []
+    arrTable = [
+        [bottom, low],
+        [bottom, average],
+        [bottom, high],
+        [middle, low],
+        [middle, average],
+        [middle, high],
+        [upper, low],
+        [upper, average],
+        [upper, high]
+    ]
+
+    for j in range(9):
+        tempNilai = min(arrTable[j][0], arrTable[j][1])
+        if (j == 1) or (j == 2)  :
+            arrAccepted.append(tempNilai)
+        elif (j == 0)   or (j == 5) or (j == 4):
+            arrConsidered.append(tempNilai)
+        elif (j == 6) or (j == 7) or (j == 8)  or (j == 3)  :
+            arrRejected.append(tempNilai)
+
+    accepted = max(arrAccepted)
+    considered = max(arrConsidered)
+    rejected = max(arrRejected)
+
+    return accepted, considered, rejected
+def defuzzyfication(inference):
+    accepted = inference[0]
+    considered = inference[1]
+    rejected = inference[2]
+    return ((accepted * 100) + (considered * 60) + (rejected * 40)) / (accepted + considered + rejected)
+
+hasilAkhir = []
+for i in range(100):
+    fuzzypenghasilan = klasifikasiPenghasilan(penghasilan[i])
+    print(fuzzypenghasilan)
+    fuzzypengeluaran = klasifikasiPengeluaran(pengeluaran[i])
+    inference = inferenceTable(fuzzypenghasilan,fuzzypengeluaran)
+    defuzz = defuzzyfication(inference)
+    hasilAkhir.append({'id': i+1,'hasil': defuzz})
+srt = sorted(hasilAkhir, key=lambda x:x['hasil'],reverse=True)
+arr = []
+for i in range(20):
+    arr.append(srt[i])
+print(arr)
