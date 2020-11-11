07a0796f56f567ec0a772ee4ccc6b49477e9d2f1
Mohammaddaffaharis <mohammaddaffaharis@gmail.com>
2020-11-11 15:42:10 +0700

    update lagi

--- FuzzyLogic.py
+++ FuzzyLogic.py
@@ -9,6 +9,8 @@ for i in file.index:
     pengeluaran.append(file['Pengeluaran'][i])
     penghasilan.append(file['Penghasilan'][i])
 
+
+# high,average,low
 def pengeluaranTinggi(pengeluaran):
     max = 10
     min = 8
@@ -40,9 +42,17 @@ def pengeluaranStandar(pengeluaran):
         return 1
     elif(pengeluaran > max and pengeluaran <= maxUnCom):
         return (maxUnCom-pengeluaran) / (maxUnCom-max)
+def klasifikasiPengeluaran(pengeluaran):
+    result = []
+    result.append(pengeluaranRendah(pengeluaran))
+    result.append(pengeluaranStandar(pengeluaran))
+    result.append(pengeluaranTinggi(pengeluaran))
+    return result
+
+#upper,middle,bottom
 def penghasilanTinggi(penghasilan):
-    max = 10
-    min = 8
+    max = 19
+    min = 14
     if(penghasilan > max):
         return 1
     elif(penghasilan > min and penghasilan <= max):
@@ -50,7 +60,7 @@ def penghasilanTinggi(penghasilan):
     elif(penghasilan <= min):
         return 0
 def penghasilanRendah(penghasilan):
-    max = 5
+    max = 7
     min = 3
     if(penghasilan > max):
         return 0
@@ -59,10 +69,10 @@ def penghasilanRendah(penghasilan):
     elif(penghasilan <= min):
         return 1
 def penghasilanStandar(penghasilan):
-    max = 7.5
-    min = 5.5
-    maxUnCom = 9
-    minUnCom = 4.5
+    max = 13.5
+    min = 7.5
+    maxUnCom = 17
+    minUnCom = 5
     if(penghasilan <= minUnCom or penghasilan > maxUnCom):
         return 0
     elif(penghasilan > minUnCom and penghasilan <= min):
@@ -71,15 +81,50 @@ def penghasilanStandar(penghasilan):
         return 1
     elif(penghasilan > max and penghasilan <= maxUnCom):
         return (maxUnCom - penghasilan) / (maxUnCom - max)
+def klasifikasiPenghasilan(penghasilan):
+    result = []
+    result.append(penghasilanRendah(penghasilan))
+    result.append(penghasilanStandar(penghasilan))
+    result.append(pengeluaranStandar(penghasilan))
+def printPengeluarannPenghasilan():
+    x = [3, 4.5, 5, 5.5, 7.5, 8, 9, 10]
+    y1 = [0, 0, 0, 0, 0, 1, 1, 1]
+    y2 = [1, 1, 1, 0, 0, 0, 0, 0]
+    y3 = [0, 0, 0, 1, 1, 0, 0, 0]
+    plt.plot(x, y1, label='Pengeluaran Tinggi')
+    plt.plot(x, y2, label='Pengeluaran Rendah')
+    plt.plot(x, y3, label='Pengeluaran Standar')
+    plt.title('Pengeluaran')
+    plt.legend()
+    plt.show()
 
-x = [3,4.5,5,5.5,7.5,8,9,10]
-y1 = [0,0,0,0,0,1,1,1]
-y2 = [1,1,1,0,0,0,0,0]
-y3 = [0,0,0,1,1,0,0,0]
+    x = [3, 5, 7, 7.5, 13.5, 14, 17, 19]
+    y1 = [0, 0, 0, 0, 0, 1, 1, 1]
+    y2 = [1, 1, 1, 0, 0, 0, 0, 0]
+    y3 = [0, 0, 0, 1, 1, 0, 0, 0]
+    plt.plot(x, y1, label='Penghasilan Tinggi')
+    plt.plot(x, y2, label='Penghasilan Rendah')
+    plt.plot(x, y3, label='Penghasilan Standar')
+    plt.title('Penghasilan')
+    plt.legend()
+    plt.show()
 
-plt.plot(x, y1, label='Pengeluaran Tinggi')
-plt.plot(x, y2, label='Pengeluaran Rendah')
-plt.plot(x, y3, label='Pengeluaran Standar')
-plt.title('Pengeluaran')
-plt.legend()
-plt.show()
+def fuzzyTable(fuzzy1, fuzzy2):
+    if (fuzzy1 == 'bottom') and (fuzzy2 == 'low'):
+        return 'considered'
+    elif (fuzzy1 == 'bottom') and (fuzzy2 == 'average'):
+        return 'accepted'
+    elif (fuzzy1 == 'bottom') and (fuzzy2 == 'high'):
+        return 'accepted'
+    elif (fuzzy1 == 'middle') and (fuzzy2 == 'low'):
+        return 'considered'
+    elif (fuzzy1 == 'middle') and (fuzzy2 == 'average'):
+        return 'considered'
+    elif (fuzzy1 == 'middle') and (fuzzy2 == 'high'):
+        return 'accepted'
+    elif (fuzzy1 == 'upper') and (fuzzy2 == 'low'):
+        return 'rejected'
+    elif (fuzzy1 == 'upper') and (fuzzy2 == 'average'):
+        return 'rejected'
+    elif (fuzzy1 == 'upper') and (fuzzy2 == 'high'):
+        return 'considered'
